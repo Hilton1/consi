c6fb8d298f0556d737643b762bb3d056c676c869
Hilton <jose.eufrasio@phooto.com.br>
2024-03-26 19:38:20 -0300

    fix: adding url to socialmedia

--- src/view/components/Footer/index.jsx
+++ src/view/components/Footer/index.jsx
@@ -5,14 +5,14 @@ export function Footer() {
   return (
     <div className="flex justify-around items-center py-4 shadow-lg-up">
       <div className='flex gap-3'>
-        <a href="">
+        <a href="http://tiktok.com/@cosi_ifal" target='_blank'>
           <img
             src={tiktok}
             alt="tiktok"
             className='max-w-6 md:max-w-8'
           />
         </a>
-        <a href="https://www.instagram.com/consi_ifal/" target='_blank'>
+        <a href="https://www.instagram.com/cosi_ifal/" target='_blank'>
           <img
             src={instagram}
             alt="Instagram"
