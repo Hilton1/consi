925809cea61592dd69bcccddfdff336aba47ee21
Hilton <jose.eufrasio@phooto.com.br>
2024-03-26 16:49:57 -0300

    feat: adding responsivity to about card

--- src/view/components/About/index.jsx
+++ src/view/components/About/index.jsx
@@ -2,15 +2,17 @@ import casal from '../../../assets/casal.svg';
 
 export function About() {
   return (
-    <div id="about" className="flex justify-around px-32 items-center pt-16">
-      <div className='w-[600px]'>
-        <img src={casal} alt="Casal" className='w-full'/>
+    <div id="about" className="lg:flex lg:justify-around px-4 lg:px-16 xl:px-32 items-center pt-16 gap-8">
+      <div className='flex justify-center xl:max-w-xl lg:max-w-lg'>
+        <div className='md:w-[500px]'>
+          <img src={casal} alt="Casal" className='w-full'/>
+        </div>
       </div>
       <div>
-        <h1 className='font-poppins font-bold text-4xl'>
+        <h1 className='font-poppins font-bold text-3xl xl:text-4xl text-center lg:text-left'>
           O que é o CONSI<span className='text-[#7AC4E0]'>?</span>
         </h1>
-        <div className='max-w-lg font-poppins text-xl mt-5'>
+        <div className='lg:max-w-lg text-justify font-poppins text-lg xl:text-xl mt-5'>
           <p className='mb-10'>
             O <b>CONSI</b> é uma iniciativa aberta e gratuita, que busca fomentar o compartilhamento de conhecimento e experiências entre os participantes.
           </p>
--- src/view/components/Description/index.jsx
+++ src/view/components/Description/index.jsx
@@ -3,20 +3,24 @@ import { Button } from '../Button';
 
 export function Description() {
   return (
-    <div id='description' className="flex flex-col lg:flex-row lg:justify-around px-4 lg:px-32 items-center py-10 lg:pt-16 shadow-lg text-center lg:text-justify">
-      <div className='max-w-xl'>
-        <h1 className='font-poppins font-bold text-3xl lg:text-4xl xl:text-7xl leading-[1.05]'>
+    <div id='description' className="flex flex-col lg:flex-row lg:justify-around px-4 lg:px-16 xl:px-32 items-center py-10 lg:pt-16 shadow-lg text-center lg:text-left">
+      <div className='xl:max-w-xl lg:max-w-lg'>
+        <h1 className='font-poppins font-bold text-3xl lg:text-6xl xl:text-7xl leading-[1.05]'>
           Colóquio de
           <span className='text-[#7AC4E0]'> Estudantes</span> de
           <span className='text-[#0E4F84]'> Sistemas</span> de
           <span className='text-[#9C9CCD]'> Informação</span>.
         </h1>
-        <p className='font-poppins font-semibold text-lg my-10 lg:my-12'>
-          Conectando entusiastas, profissionais e curiosos do mundo de tecnologia.
-        </p>
+
+        <div className='lg:text-left'>
+          <p className='font-poppins font-semibold text-lg my-10 lg:w-[85%] lg:my-12'>
+            Conectando entusiastas, profissionais e curiosos do mundo de tecnologia.
+          </p>
+        </div>
+
         <Button text='Quero participar do evento'/>
       </div>
-      <div className='md:w-[600px]'>
+      <div className='md:w-[500px]'>
         <img src={empresa} alt="Empresa" className='w-full'/>
       </div>
     </div>
