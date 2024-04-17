df72e3aac62a08dcbf8ad84ccf7911a8df5ef8af
Hilton <jose.eufrasio@phooto.com.br>
2024-04-17 20:55:49 -0300

    feat: adicionando patrocinadores

--- src/view/components/Sponsors/components/Card.jsx
+++ src/view/components/Sponsors/components/Card.jsx
@@ -2,18 +2,18 @@ export function Card({name, image, type}) {
   return (
     <div className='max-w-52 text-center'>
       <div className='flex justify-center'>
-        <img src={image} alt="Foto Patrocinador" />
+      <div className="rounded-full overflow-hidden w-40 h-40 mb-4">
+          <img src={image} alt="Foto Patrocinador" className="w-full h-full object-cover rounded-full" />
+        </div>
       </div>
 
       <div className='flex justify-center'>
         <div
           className={`
-            flex items-center justify-center gap-2 rounded-full w-40
+            flex items-center justify-center gap-2 rounded-full w-48
             ${type === 1 ? 'bg-organization' : 'bg-sponsorship'}
           `}
         >
-          <p className='p-3 bg-white rounded-full'></p>
-
           <p className='font-poppins py-2'>
             { name }
           </p>
--- src/view/components/Sponsors/index.jsx
+++ src/view/components/Sponsors/index.jsx
@@ -1,30 +1,13 @@
 import { Card } from './components/Card';
 
-import gallery_purple from '../../../assets/gallery_purple.svg';
+import mesha from '../../../assets/mesha.jpg';
 
 import 'swiper/css';
 
 export function Sponsors() {
 
   const people = [
-    { img: gallery_purple, name: 'Patrocinador X', type: 2 },
-    { img: gallery_purple, name: 'Patrocinador Y', type: 2 },
-    { img: gallery_purple, name: 'Patrocinador Z', type: 2 },
-    { img: gallery_purple, name: 'Patrocinador X', type: 2 },
-    { img: gallery_purple, name: 'Patrocinador Y', type: 2 },
-    { img: gallery_purple, name: 'Patrocinador Z', type: 2 },
-    { img: gallery_purple, name: 'Patrocinador X', type: 2 },
-    { img: gallery_purple, name: 'Patrocinador Y', type: 2 },
-    { img: gallery_purple, name: 'Patrocinador Z', type: 2 },
-    { img: gallery_purple, name: 'Patrocinador X', type: 2 },
-    { img: gallery_purple, name: 'Patrocinador Y', type: 2 },
-    { img: gallery_purple, name: 'Patrocinador Z', type: 2 },
-    { img: gallery_purple, name: 'Patrocinador X', type: 2 },
-    { img: gallery_purple, name: 'Patrocinador Y', type: 2 },
-    { img: gallery_purple, name: 'Patrocinador Z', type: 2 },
-    { img: gallery_purple, name: 'Patrocinador X', type: 2 },
-    { img: gallery_purple, name: 'Patrocinador Y', type: 2 },
-    { img: gallery_purple, name: 'Patrocinador Z', type: 2 },
+    { img: mesha, name: 'Mesha. tecnologia', type: 2 },
   ];
 
   return (
@@ -33,7 +16,12 @@ export function Sponsors() {
       className="py-32 px-4 lg:px-16 xl:px-32 shadow-lg-up relative h-full"
     >
       <div className="text-center">
-        <h1 className="font-poppins font-bold text-4xl mb-10">Patrocinadores do evento<span className='text-[#7AC4E0]'>.</span></h1>
+        <h1 className="font-poppins font-bold text-4xl mb-4">Patrocinadores do evento<span className='text-[#7AC4E0]'>.</span></h1>
+        <div className='font-poppins text-lg text-justify md:text-center lg:text-justify lg:text-xl mt-5'>
+          <p className='mb-10 text-center'>
+            Confira abaixo as empresas que estão apoiando o nosso evento!
+          </p>
+        </div>
         <div className="grid gap-4 justify-center items-center grid-cols-2 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6">
           {
             people.map((person) => (
